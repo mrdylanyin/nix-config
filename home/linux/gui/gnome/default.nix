03e8108dc3586b4a3f26123dacdb70b9bf4d8680
mrdylanyin <mrdylanyin@gmail.com>
2024-09-05 20:34:05 +0800

    fix

--- home/linux/gui/gnome/default.nix
+++ home/linux/gui/gnome/default.nix
@@ -2,20 +2,23 @@
   pkgs,
   config,
   lib,
-  anyrun,
   ...
-}: {
-  config = lib.mkIf config.modules.desktop.gnome.enable {
-    # GNOME 相关的 home-manager 配置
+} @ args:
+with lib; let
+  cfg = config.modules.desktop.gnome;
+in {
+  options.modules.desktop.gnome = {
+    enable = mkEnableOption "GNOME 桌面环境";
+  };
+
+  config = mkIf cfg.enable {
     dconf.settings = {
-      # 示例：设置默认主题
       "org/gnome/desktop/interface" = {
         gtk-theme = "Adwaita-dark";
       };
     };
 
     home.packages = with pkgs; [
-      # 添加您想要的 GNOME 应用或工具
       gnome.gnome-tweaks
       gnome.dconf-editor
     ];
